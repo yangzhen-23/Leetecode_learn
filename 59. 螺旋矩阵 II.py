f9b46022fbc4b5e00c5b2a967857e5c9bf8006f1
YangZhen <yangnade23@gmail.com>
2026-07-24 21:24:39 +0800

    水

--- 59. 螺旋矩阵 II.py
+++ 59. 螺旋矩阵 II.py
@@ -33,3 +33,20 @@ if __name__ == '__main__':
     sol = Solution()
     print(sol.generateMatrix(n))
 
+"""
+# 思路
+    1. 创建一个 n x n 的矩阵 mat，并初始化为 0。
+    2. 使用一个计数器 count 来跟踪当前要填充的数字，从  1 开始。
+    3. 使用一个 start_index 来表示当前螺旋的起始位置。
+    4. 使用一个 while 循环，直到 count 大于等于 n^2。
+    5. 在每一层螺旋中，按顺时针方向填充矩阵。
+        - 先填充顶部行，从 start_index 开始，到 n - 1 - start_index。
+        - 然后填充右侧列，从 start_index 开始，到 n - 1 - start_index。
+        - 最后填充底部行，从 n - 1 - start_index 开始，到 start_index。
+        - 最后填充左侧列，从 n - 1 - start_index 开始，到 start_index。
+        - 增加 start_index，准备填充下一层螺旋。
+    6. 如果 n 是奇数，则在矩阵的中心位置填充最后一个数字。
+        - 计算中心位置的索引，即 n // 2。
+        - 将最后一个数字 count 赋值给 mat[n // 2][n。 
+        - 返回 mat。
+"""
