a29b223f95e31710e12ad5019988ad430e40e62b
YangZhen <yangnade23@gmail.com>
2026-08-28 01:40:35 +0800

    LC_2

---
+++ 苏大机试/LC_1.py
@@ -0,0 +1,15 @@
+"""
+1. 两数之和
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+
+你可以按任意顺序返回答案。
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            res = target - nums[i]
+            if res in nums[i+1:]:
+                return [i, nums[i+1:].index(res)+i+1]
